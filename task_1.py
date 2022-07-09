a1263d859c9bc1ebadd67801ca68142b1a84fe52
Buanzu <mitrofanovmv@yandex.ru>
2022-07-09 17:46:17 +1000

    Домашнее задание урок 1

---
+++ task_1.py
@@ -0,0 +1,6 @@
+my_name = "Maxim"
+my_age = "37"
+my_profession = "Онколог"
+my_IT_skills = "Чайник"
+print(f"Your name {my_name} and your age {my_age}")
+print(f"Your profession {my_profession} and your IT_skills {my_IT_skills}.")
