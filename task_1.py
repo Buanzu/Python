b4b9242dec80a646a267b9c1e795b82ae9830bd2
Buanzu <mitrofanovmv@yandex.ru>
2022-07-17 14:16:57 +1000

    Слив 2 ветки

--- task_1.py
+++ task_1.py
@@ -1,6 +1,16 @@
-my_name = "Maxim"
-my_age = "37"
-my_profession = "Онколог"
-my_IT_skills = "Чайник"
-print(f"Your name {my_name} and your age {my_age}")
-print(f"Your profession {my_profession} and your IT_skills {my_IT_skills}.")
+my_list = [5, 4, 3, 2, 1]
+print(f"Рейтинг - {my_list}")
+digit = int(input("Введите число (000 - выход) "))
+while digit != 000:
+    for el in range(len(my_list)):
+        if my_list[el] == digit:
+            my_list.insert(el + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[el] > digit and my_list[el + 1] < digit:
+            my_list.insert(el + 1, digit)
+    print(f"текущий список - {my_list}")
+    digit = int(input("Введите число (000 - выход) "))
