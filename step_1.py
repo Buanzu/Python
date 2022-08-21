b798ce71ceacb17e1ccd8b669407393ef6ba9cc1
Buanzu <mitrofanovmv@yandex.ru>
2022-08-21 19:49:24 +1000

    End_first_part.

--- step_1.py
+++ step_1.py
@@ -1,19 +1,4 @@
-my_list = [7, 5, 4, 3, 1]
-print(f"Рейтинг - {my_list}")
-digit = int(input("Введите число (00 - выход) "))
-while digit != 00:
-    for element in range(len(my_list)):
-        if my_list[element] == digit:
-            my_list.insert(element + 1, digit)
-            break
-        elif my_list[0] < digit:
-            my_list.insert(0, digit)
-        elif my_list[-1] > digit:
-            my_list.append(digit)
-        elif my_list[element] > digit and my_list[element + 1] < digit:
-            my_list.insert(element + 1, digit)
-    print(f"Текущий список - {my_list}")
-    digit = int(input("Введите число (00 - выход) "))
+print("Hello world")
 
 
 
